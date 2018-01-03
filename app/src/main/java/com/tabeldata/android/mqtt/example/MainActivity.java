8bad277194cf6134f4b95fdaf21d7e7c045e04fb
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-03 17:58:39 +0700

    connect to cloudmqtt.com

--- app/src/main/java/com/tabeldata/android/mqtt/example/MainActivity.java
+++ app/src/main/java/com/tabeldata/android/mqtt/example/MainActivity.java
@@ -9,43 +9,54 @@ import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void connectTo(View view) {
+    public MqttAndroidClient getConnection(String brokerServer, String username, char[] password) throws MqttException {
         String clientId = MqttClient.generateClientId();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password);
+
         MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.1.15:1883",
-                        clientId);
+                new MqttAndroidClient(this.getApplicationContext(), brokerServer, clientId);
 
+        IMqttToken token = client.connect(options);
+        token.setActionCallback(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                // We are connected
+                Log.i("connection", "onSuccess");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                // Something went wrong e.g. connection timeout or firewall problems
+                Log.e("comunitationError", exception.getMessage());
+
+            }
+        });
+        return client;
+    }
+
+    public void connectTo(View view) {
         try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    Log.i("connection", "onSuccess");
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.e("comunitationError", exception.getMessage());
-
-                }
-            });
+            MqttAndroidClient connection = getConnection(
+                    "tcp://m14.cloudmqtt.com:13568",
+                    "rtyoepgj",
+                    "f6kj3JqI7nAv".toCharArray());
         } catch (MqttException e) {
             e.printStackTrace();
         }
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
 }
